10075a3c1768017092530677d59691164dc4247a
Mainak Chhari <mainakchhari.iitk@gmail.com>
2021-08-24 20:24:42 +0530

    22 August 2021

--- ngos/models.py
+++ ngos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
 
-# Create your models here.
+
+class Ngo(models.Model):
+    name = CharField(max_length=25, null=False)
+    registration_no = CharField(max_length=16, null=False, primary_key=True)
+    address = TextField(null=False)
+    description = TextField(null=True, blank=True)  # TODO: why blank=True
